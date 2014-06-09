8a44864b02cb9cc384b9dabedf17297cbbfe441c
John-Holt-Tessella <John.Holt@tessella.com>
2014-06-09 14:13:20 +0000

    JOJ5: validate namelist files, namelists and parameters

--- job_runner/job_runner/controllers/jobs.py
+++ job_runner/job_runner/controllers/jobs.py
@@ -1,32 +1,74 @@
+#header
+
 import logging
 
-from pylons import request, response, session, tmpl_context as c, url, config
-from pylons.controllers.util import abort, redirect
+from pylons import request
+from pylons.controllers.util import abort
 from pylons.decorators import jsonify
 from simplejson import JSONDecodeError
 
-from job_runner.lib.base import BaseController, render
+from job_runner.lib.base import BaseController
 from job_runner.utils import constants
 
 log = logging.getLogger(__name__)
 
+
+def _validate_namelist(namelist):
+    """
+    Validate that the namelist has a name and that the parameters have names
+    """
+    if not 'name' in namelist or namelist['name'].strip() == '':
+            abort(400, "Invalid name for one of the name lists")
+
+    if not 'parameters' in namelist:
+            abort(400, "namelist has no parameters in")
+
+    for key in namelist['parameters']:
+        if key.strip() == '':
+            abort(400, "A parameter name can not be blank")
+
+
+def _validate_namelist_file(namelist_file):
+    """
+    Validate that the namelist file has a filename and contains some namelists
+    """
+    if not 'filename' in namelist_file or namelist_file['filename'].strip() == '':
+            abort(400, "Invalid filename for one of the namelist files")
+
+    if not 'namelists' in namelist_file or len(namelist_file['namelists']) == 0:
+            abort(400, "namelist file has no namelists in")
+
+    for namelist in namelist_file['namelists']:
+        _validate_namelist(namelist)
+
 class JobsController(BaseController):
+    """
+    Controller for jobs
+    """
 
     @jsonify
     def new(self):
+        """
+        Create a new job submission.
+        """
 
+        json = {}
         try:
             json = request.json_body
         except JSONDecodeError:
-            return abort(400, "Only valid JSON Posts allowed")
+            abort(400, "Only valid JSON Posts allowed")
 
         if not 'code_version' in json or json['code_version'] not in constants.VALID_CODE_VERSIONS:
-            return abort(400, "Invalid code version")
+            abort(400, "Invalid code version")
 
         if not 'model_run_id' in json or json['model_run_id'].strip() == '':
-            return abort(400, "Invalid model run id")
+            abort(400, "Invalid model run id")
 
         if not 'namelist_files' in json or len(json['namelist_files']) == 0:
-            return abort(400, "Invalid namelist files")
+            abort(400, "Invalid namelist files")
+
+        namelist = []
+        for namelist_file in json['namelist_files']:
+            namelist.append(_validate_namelist_file(namelist_file))
 
         return '100'
--- job_runner/job_runner/tests/functional/test_jobs.py
+++ job_runner/job_runner/tests/functional/test_jobs.py
@@ -1,12 +1,26 @@
 from job_runner.tests import *
 from hamcrest import *
 
+NAMELISTS = 'namelists'
 
 
 class TestJobsController(TestController):
 
     def setUp(self):
-        namelist_files = ['file1']
+        namelist_files = \
+            [
+                {'filename':'file1',
+                 NAMELISTS:
+                     [
+                         {'name':'a name',
+                          'parameters':
+                              {
+                                  'time_lens': '1'
+                              }
+                         }
+                     ]
+                 }
+            ]
         self.valid_job_submission = {
             'code_version': 'Jules v3.4.1',
             'model_run_id': '101',
@@ -80,15 +94,86 @@ class TestJobsController(TestController):
         assert_that(response.status_code, is_(400), "invalid request")
         assert_that(response.normal_body, contains_string('namelist files'), "invalid request")
 
-    def test_GIVEN_namelist_file_set_is_empty_WHEN_post_new_job_THEN_error(self):
-        self.valid_job_submission['namelist_files'] = []
+    def test_GIVEN_namelist_file_has_no_filename_WHEN_post_new_job_THEN_error(self):
+        del self.valid_job_submission['namelist_files'][0]['filename']
         response = self.app.post_json(
             url(controller='jobs', action='new'),
             params=self.valid_job_submission,
             expect_errors=True)
 
         assert_that(response.status_code, is_(400), "invalid request")
-        assert_that(response.normal_body, contains_string('namelist files'), "invalid request")
+        assert_that(response.normal_body, contains_string('filename'), "invalid request")
+
+    def test_GIVEN_namelist_file_has_blank_filename_WHEN_post_new_job_THEN_error(self):
+        self.valid_job_submission['namelist_files'][0]['filename'] = ''
+        response = self.app.post_json(
+            url(controller='jobs', action='new'),
+            params=self.valid_job_submission,
+            expect_errors=True)
+
+        assert_that(response.status_code, is_(400), "invalid request")
+        assert_that(response.normal_body, contains_string('filename'), "invalid request")
+
+
+    def test_GIVEN_namelist_set_is_empty_WHEN_post_new_job_THEN_error(self):
+        self.valid_job_submission['namelist_files'][0][NAMELISTS] = []
+        response = self.app.post_json(
+            url(controller='jobs', action='new'),
+            params=self.valid_job_submission,
+            expect_errors=True)
+
+        assert_that(response.status_code, is_(400), "invalid request")
+        assert_that(response.normal_body, contains_string('namelist'), "invalid request")
+
+    def test_GIVEN_namelist_is_not_present_WHEN_post_new_job_THEN_error(self):
+        del self.valid_job_submission['namelist_files'][0][NAMELISTS]
+        response = self.app.post_json(
+            url(controller='jobs', action='new'),
+            params=self.valid_job_submission,
+            expect_errors=True)
+
+        assert_that(response.status_code, is_(400), "invalid request")
+        assert_that(response.normal_body, contains_string('namelist'), "invalid request")
+
+    def test_GIVEN_namelist_has_no_name_WHEN_post_new_job_THEN_error(self):
+        del self.valid_job_submission['namelist_files'][0][NAMELISTS][0]['name']
+        response = self.app.post_json(
+            url(controller='jobs', action='new'),
+            params=self.valid_job_submission,
+            expect_errors=True)
+
+        assert_that(response.status_code, is_(400), "invalid request")
+        assert_that(response.normal_body, contains_string('name'), "invalid request")
+
+    def test_GIVEN_namelist_has_blank_name_WHEN_post_new_job_THEN_error(self):
+        self.valid_job_submission['namelist_files'][0][NAMELISTS][0]['name'] = ''
+        response = self.app.post_json(
+            url(controller='jobs', action='new'),
+            params=self.valid_job_submission,
+            expect_errors=True)
+
+        assert_that(response.status_code, is_(400), "invalid request")
+        assert_that(response.normal_body, contains_string('name'), "invalid request")
+
+    def test_GIVEN_parameter_set_is_not_present_WHEN_post_new_job_THEN_error(self):
+        del self.valid_job_submission['namelist_files'][0][NAMELISTS][0]['parameters']
+        response = self.app.post_json(
+            url(controller='jobs', action='new'),
+            params=self.valid_job_submission,
+            expect_errors=True)
+
+        assert_that(response.status_code, is_(400), "invalid request")
+        assert_that(response.normal_body, contains_string('parameters'), "invalid request")
+
+    def test_GIVEN_parameter_name_is_blank_post_new_job_THEN_error(self):
+        self.valid_job_submission['namelist_files'][0][NAMELISTS][0]['parameters'][''] = 'val'
+        response = self.app.post_json(
+            url(controller='jobs', action='new'),
+            params=self.valid_job_submission,
+            expect_errors=True)
+
+        assert_that(response.status_code, is_(400), "invalid request")
+        assert_that(response.normal_body, contains_string('parameter name'), "invalid request")
 
 
 
