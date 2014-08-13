456e30c37db7a0aa616bd1c8cfaa019a85cbe402
matt-kendall-tessella <Matthew.Kendall@Tessella.com>
2014-08-13 17:26:33 +0100

    Fix problem with old driving dataset parameters not being overwritten when old driving dataset was user uploaded

--- majic/joj/tests/test_with_create_full_model_run.py
+++ majic/joj/tests/test_with_create_full_model_run.py
@@ -10,6 +10,7 @@ from joj.services.job_status_updater import JobStatusUpdaterService
 from joj.services.email_service import EmailService
 from joj.services.job_runner_client import JobRunnerClient
 from pylons import config
+from tests.functional.test_model_run_driving_data import TestModelRunDrivingData
 
 
 class TestWithFullModelRun(TestController):
@@ -112,6 +113,67 @@ class TestWithFullModelRun(TestController):
         self.lon_w, self.lon_e = -100, 120
         self.date_start = datetime.datetime(1907, 1, 1, 0, 0, 0)
         self.date_end = datetime.datetime(1914, 1, 1, 0, 0, 0)
+        self.app.post(url(controller='model_run', action='extents'),
+                      params={
+                          'submit': u'Next',
+                          'site': u'single',
+                          'lat': self.lat_n,
+                          'lon': self.lon_e,
+                          'start_date': self.date_start.strftime("%Y-%m-%d"),
+                          'end_date': self.date_end.strftime("%Y-%m-%d")
+                      })
+        # The Output Variables page
+        self.app.post(url(controller='model_run', action='output'),
+                      params={
+                          'submit': u'Next',
+                          'ov_select_6': 1,
+                          'ov_timestep_6': 1,
+                          'ov_monthly_6': 1,
+                          'ov_select_11': 1,
+                          'ov_yearly_11': 1,
+                          'ov_monthly_11': 1
+                      })
+
+    def create_model_run_with_user_uploaded_driving_data(self):
+        # Set up the model as if we'd gone through all the previous pages
+        # and uploaded our own driving data
+        # The Create page
+        self.model_name = u'Run with my own driving data'
+        self.model_description = u'This is a description of a model_run'
+        self.science_config = self.model_run_service.get_scientific_configurations()[2]
+        model_science_config_id = self.science_config['id']
+        self.app.post(url=url(controller='model_run', action='create'),
+                      params={
+                          'name': self.model_name,
+                          'science_configuration': str(model_science_config_id),
+                          'description': self.model_description
+                      })
+        # The Driving Data page
+        self.sample_file_contents = "# solar   long  rain  snow    temp   wind     press      humid\n" \
+                                    "# sw_down   lw_down  tot_rain  tot_snow    t   wind     pstar      q\n" \
+                                    "# i   i  i  i    i   i     i      i\n" \
+                                    "3.3  187.8   0.0   0.0  259.10  3.610  102400.5  1.351E-03\n" \
+                                    "89.5  185.8   0.0   0.0  259.45  3.140  102401.9  1.357E-03\n" \
+                                    "142.3  186.4   0.0   0.0  259.85  2.890  102401.0  1.369E-03\n" \
+                                    "# ----- data for later times ----"
+        data_service = DatasetService()
+        ds_id = data_service.get_id_for_user_upload_driving_dataset()
+        self.app.post(
+            url(controller='model_run', action='driving_data'),
+            params={
+                'driving_dataset': ds_id,
+                'submit': u'Upload',
+                'lat': u'55',
+                'lon': u'45',
+                'dt_start': u'2000-01-01 00:00',
+                'dt_end': u'2000-01-01 02:00'},
+            upload_files=[('driving-file', 'file.txt', self.sample_file_contents)]
+        )
+        # The Extents page
+        self.lat_n, self.lat_s = 55, 55
+        self.lon_w, self.lon_e = 45, 45
+        self.date_start = datetime.datetime(2000, 1, 1, 0, 0, 0)
+        self.date_end = datetime.datetime(2000, 1, 1, 0, 2, 0)
         self.app.post(url(controller='model_run', action='extents'),
                       params={
                           'submit': u'Next',
