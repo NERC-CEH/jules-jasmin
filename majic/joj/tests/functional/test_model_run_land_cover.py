a014ed120e12f5d51eb11ddd7eaae8ebfe8deaf9
John-Holt-Tessella <John.Holt@tessella.com>
2014-09-25 15:21:37 +0000

    Set headers and license

--- majic/joj/tests/functional/test_model_run_land_cover.py
+++ majic/joj/tests/functional/test_model_run_land_cover.py
@@ -1,5 +1,20 @@
 """
-header
+#    Majic
+#    Copyright (C) 2014  CEH
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
 from urlparse import urlparse
 from hamcrest import is_, assert_that, contains_string, is_not, string_contains_in_order
