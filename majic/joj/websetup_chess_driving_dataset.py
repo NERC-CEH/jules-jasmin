2dfc9e330690c89ffa60a319ebf92c7e5f8d75f6
John-Holt-Tessella <John.Holt@tessella.com>
2014-09-25 08:26:18 +0000

    Set Chess post process to 0

--- majic/joj/websetup_chess_driving_dataset.py
+++ majic/joj/websetup_chess_driving_dataset.py
@@ -152,7 +152,7 @@ def _create_chess_parameters_and_locations(cover_dst, land_cover_frac_dst, soild
 
         [constants.JULES_PARAM_SURF_HGT_ZERO_HEIGHT, ".true."],
 
-        [constants.JULES_PARAM_POST_PROCESSING_ID, "2"],
+        [constants.JULES_PARAM_POST_PROCESSING_ID, "0"],
 
         [constants.JULES_PARAM_SWITCHES_L_VG_SOIL, ".true."]
     ]
