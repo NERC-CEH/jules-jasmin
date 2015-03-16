0c14a46bececfe3ff95abda809e4f7bac1494bcc
john-holt-tessella <John.Holt@Tessella.com>
2015-03-16 16:29:17 +0000

    JOJ-145:Add build agent script. Add teamcity job for releasing majic

--- majic/configuration/server/teamcity_job_release_majic.sh
+++ majic/configuration/server/teamcity_job_release_majic.sh
@@ -11,8 +11,8 @@ then
     sudo -u apache rm -rf "$MAJIC_SOURCE_PATH"
 fi
 
-sudo -u apache mkdir "$MAJIC_SOURCE_PATH" || echo "Can not make majic path" || exit 1
-sudo -u apache cp -r . "$MAJIC_SOURCE_PATH" || echo "Can not copy majic" || exit 2
+sudo -u apache mkdir "$MAJIC_SOURCE_PATH" || ( echo "Can not make majic path" && exit 1 )
+sudo -u apache cp -r . "$MAJIC_SOURCE_PATH" || ( echo "Can not copy majic" && exit 2 )
 cd "$MAJIC_SOURCE_PATH"
 
 
