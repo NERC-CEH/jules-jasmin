745ca8550beeaf644a50a55360981a50b19e9bd2
john-holt-tessella <John.Holt@Tessella.com>
2015-03-17 14:25:18 +0000

    JOJ-145: Better diff error message

--- majic/configuration/server/teamcity_job_release_check_files.sh
+++ majic/configuration/server/teamcity_job_release_check_files.sh
@@ -3,18 +3,21 @@
 diff -w /etc/httpd/conf.d/z_majic_runner.conf majic/configuration/apache/z_majic_runner.conf
 if [ $? -ne 0 ]
 then
+   echo "z_majic_runner.conf files differ"
    exit 1
 fi
 
 diff -w /etc/cron.d/job_status_updater majic/configuration/server/job_status_updater
 if [ $? -ne 0 ]
 then
+   echo "job_status_updater files differ"
    exit 1
 fi
 
 diff -w /etc/cron.d/sql_db_backup majic/configuration/server/sql_db_backup
 if [ $? -ne 0 ]
 then
+   echo "sql_db_backup files differ"
    exit 1
 fi
 
