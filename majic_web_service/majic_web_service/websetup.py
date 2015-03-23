957217b8402bf7e26baa43f3c189eb84f632e693
john-holt-tessella <John.Holt@Tessella.com>
2015-03-23 15:39:27 +0000

    JOJ-141: Create web services from template. Add setup for pylons

---
+++ majic_web_service/majic_web_service/websetup.py
@@ -0,0 +1,15 @@
+"""Setup the majic_web_service/ application"""
+import logging
+
+from majic_web_service.config.environment import load_environment
+from majic_web_service.model.meta import Session, Base
+
+log = logging.getLogger(__name__)
+
+def setup_app(command, conf, vars):
+    """Place any commands to setup majic_web_service here"""
+    # Don't reload the app if it was loaded under the testing environment
+    load_environment(conf.global_conf, conf.local_conf)
+
+    # Create the tables if they don't already exist
+    Base.metadata.create_all(bind=Session.bind)
