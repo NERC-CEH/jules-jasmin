92c4368eba3a233d5d96e09f62e3242623ab97b8
john-holt-tessella <John.Holt@Tessella.com>
2015-04-09 16:33:47 +0100

    JOJ161: Perform dump before database backup

--- majic/configuration/server/teamcity_job_update_majic_database.sh
+++ majic/configuration/server/teamcity_job_update_majic_database.sh
@@ -10,5 +10,18 @@ cd majic
 sed -i "s#sqlalchemy.url.*#sqlalchemy.url = mysql+mysqlconnector://joj_admin:${DB_PASSWORD}@localhost/joj#g" $CONFIGURATION
 sed -i "s/password_template/${CROWD_PASSWORD}/g" $CONFIGURATION
 
+dump_filename="$PWD/dump_filename"
+mysqldump --databases joj --single-transaction -u joj_admin -p"DB_PASSWORD" --result-file $dump_filename
+EXITCODE=$?
+if [ $EXITCODE -ne 0 ] ; then
+  echo "Database backup Failed."
+  echo "Database backup Failed. Check the reason why, see file $dump_filename"
+  exit -1
+else
+  echo "Database backup Success. Dump is at $dump_filename"
+fi
+
+echo "Database dump created in $PWD/dump_filename"
+
 source $VENV/bin/activate
 alembic -c $CONFIGURATION upgrade head
