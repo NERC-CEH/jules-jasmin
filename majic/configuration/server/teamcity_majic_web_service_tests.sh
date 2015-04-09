991b9600cf14ff98d926e8a0d7674a509a07aab1
john-holt-tessella <John.Holt@Tessella.com>
2015-04-09 09:46:50 +0100

    JOJ161: Update test user name

--- majic/configuration/server/teamcity_majic_web_service_tests.sh
+++ majic/configuration/server/teamcity_majic_web_service_tests.sh
@@ -5,7 +5,7 @@ VENV="/var/local/majic_ws_test/venv"
 source $VENV/bin/activate
 
 cd majic_web_service
-sed -i "s#sqlalchemy.url.*#sqlalchemy.url = mysql+mysqlconnector://majic_ws_test_admin:$1@localhost/majic_ws_test#g" test.ini
+sed -i "s#sqlalchemy.url.*#sqlalchemy.url = mysql+mysqlconnector://majic_ws_test:$1@localhost/majic_ws_test#g" test.ini
 
 $VENV/bin/paster setup-app test.ini
 $VENV/bin/python setup.py test
