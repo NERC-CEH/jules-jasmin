380c08d8cf7faff7c95f18d379eefc58e4fcccd3
john-holt-tessella <John.Holt@Tessella.com>
2015-04-13 17:18:09 +0100

    Make db update script use configuration from majic

--- majic/configuration/server/teamcity_job_update_majic_database.sh
+++ majic/configuration/server/teamcity_job_update_majic_database.sh
@@ -7,10 +7,8 @@ MAJIC_SOURCE_PATH="/var/local/majic/jules-jasmin"
 VENV="/var/local/majic/virtual_env"
 
 cd majic
-sed -i "s#sqlalchemy.url.*#sqlalchemy.url = mysql+mysqlconnector://joj_admin:${DB_PASSWORD}@localhost/joj#g" $CONFIGURATION
-sed -i "s/password_template/${CROWD_PASSWORD}/g" $CONFIGURATION
 
 sudo -u mysql /var/local/majic/jules-jasmin/majic/joj/cron_scripts/database_backup_and_maintenance.sh ${DB_PASSWORD} || exit 1
 
 source $VENV/bin/activate
-alembic -c $CONFIGURATION upgrade head
+alembic -c $MAJIC_SOURCE_PATH/majic/$CONFIGURATION upgrade head
